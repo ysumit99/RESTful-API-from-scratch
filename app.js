5121d185a212c561a30f7d1cad02d83fbdfc7c8f
sumit23 <ysumit99@gmail.com>
2018-07-30 02:31:42 +0530

    using RestEasy Chrome extension for testing POST requests

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+app.use(bodyParser.json());
+
 Genre = require('./models/genre');
+Book = require('./models/book');
+
+
 //Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
 var db = mongoose.connection;
@@ -21,6 +26,45 @@ app.get('/api/genres',function(req, res){
 		res.json(genres);
 	});
 });
+
+app.post('/api/genres',function(req, res){
+	var genre = req.body;
+	Genre.addGenres(genre, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.get('/api/books',function(req, res){
+	Book.getBooks(function(err, books){
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id',function(req, res){
+	Book.getBookById(req.params._id, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books',function(req, res){
+	var book = req.body;
+	Book.addBook(book, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
 
 
 app.listen(3000);
---
+++ models/book.js
@@ -0,0 +1,58 @@
+var mongoose =  require('mongoose');
+
+// Book Schema
+var bookSchema = mongoose.Schema({
+	title:{
+		type: String,
+		require: true
+	},
+	genre:{
+		type: String,
+		require: true
+	},
+	description:{
+		type: String,
+		
+	},
+	author:{
+		type: String,
+		require: true
+	},
+	publisher:{
+		type: String,
+		//require: true
+	},
+	pages:{
+		type: String,
+		
+	},
+	image_url:{
+		type: String,
+		//require: true
+	},
+	buy_url:{
+		type: String,
+		//require: true
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Book = module.exports = mongoose.model('Book', bookSchema);
+
+//Get Books
+module.exports.getBooks = function(callback, limit){
+	Book.find(callback).limit(limit);
+}
+
+//Get Book
+module.exports.getBookById = function(id, callback){
+	Book.findById(id, callback);
+}
+
+//Add Book
+module.exports.addBook = function(book, callback){
+	Book.create(book, callback);
+}
